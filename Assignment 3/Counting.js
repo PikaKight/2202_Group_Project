1cd94447840e25a6a662c5a4a304404f6614384f
PikaKight <tmmarcus73@gmail.com>
2021-12-08 22:29:42 -0500

    got it really close to the answer

--- Assignment 3/Counting.js
+++ Assignment 3/Counting.js
@@ -1,35 +1,36 @@
-function counting(reading){
+function counting(reading) {
     const words = require("./words.json")
-    let [synonyms,related,near_antonyms,antonyms] = [0,0,0,0]; 
-    for (i = 0; i < reading.length; i++){
-        reading[i] = reading[i].replace("(", "").replace(")", "").replace("\"", "").replace(",","").replace(".","").replace(":","").replace("...", "").replace("?","").replace(";", "");
-        if (reading[i] in words["Synonyms"]){
+    let [synonyms, related, near_antonyms, antonyms] = [0, 0, 0, 0];
+    for (i = 0; i < reading.length; i++) {
+        reading[i] = reading[i].replace("(", "").replace(")", "").replace("\"", "").replace(",", "").replace(".", "").replace(":", "").replace("...", "").replace("?", "").replace(";", "");
+
+        if (words["Synonyms"].includes(reading[i])) {
             synonyms += 1;
         }
-        if (reading[i] in words["Related"]){
-            related +=1;
+        if (words["Related"].includes(reading[i])) {
+            related += 1;
         }
-        if (reading[i] in words["Near Antonyms"]){
+        if (words["Near Antonyms"].includes(reading[i])) {
             near_antonyms += 1;
         }
-        if (reading[i] in words["Antonyms"]){
+        if (words["Antonyms"].includes(reading[i])) {
             antonyms += 1;
         }
     }
 
-    return [synonyms, related, near_antonyms, antonyms];    
+    return [synonyms, related, near_antonyms, antonyms];
 }
 
 module.exports = counting;
 
 if (require.main === module) {
     const fs = require('fs');
-    fs.readFile('Assignment 3/Optimism_and_your_health.txt', 'utf8' , (err, data) => {
+    fs.readFile('Assignment 3/Optimism_and_your_health.txt', 'utf8', (err, data) => {
         if (err) {
-          console.error(err)
-          return
+            console.error(err)
+            return
         }
         text = data.split(/\s+/g);
         console.log(counting(text))
-      })
+    })
 }
