54cfd0787726c83fe590a3b2ee6b1b398144ab87
bnelso4 <90856449+bnelso4@users.noreply.github.com>
2021-12-08 23:15:47 -0500

    added counting

--- Assignment 3/ReadingFrom.js
+++ Assignment 3/ReadingFrom.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const counting = require("./Counting");
 let readingPromise =fs.promises.readFile('./Assignment 3/Optimism_and_your_health.txt');
 readingPromise.then(function(result)
 {
@@ -6,9 +7,11 @@ readingPromise.then(function(result)
     let reToString =result.toString('utf-8');
 
     //you can remove this whenever you want
-    console.log(reToString);
-
+    
+    let fileArray = reToString.split(/\s+/g);
+    
     //add call to counter function or the actual function here
+    let countedArray = counting(fileArray);
     
 }).catch(function(error)
 {
