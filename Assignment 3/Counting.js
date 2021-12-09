1e772d9281dc79edaad01dbd385247d6f1c5379b
pikakight <tmmarcus73@gmail.com>
2021-12-08 21:16:43 -0500

    getting some testing done

--- Assignment 3/Counting.js
+++ Assignment 3/Counting.js
@@ -2,23 +2,28 @@ const { count } = require("console");
 
 function counting(reading){
     const words = require("./words.json")
-    let [synonyms, related, near_antonyms, antonyms] = [0,0,0,0]; 
-    reading.forEach(element => {
-        if (element in words.Synonyms){
+    let synonyms  = 0; 
+    let related = 0;
+    let near_antonyms = 0;
+    let antonyms = 0; 
+    i = 0;
+    for (element of reading){
+        if (element in words["Synonyms"]){
             synonyms += 1;
         }
-        else if (element in words.Related){
+        else if (element in words["Related"]){
             related +=1;
         }
         else if (element in words["Near Antonyms"]){
             near_antonyms += 1;
         }
-        else if (element in words.Antonyms){
+        else if (element in words["Antonyms"]){
             antonyms += 1;
         }
-    });
+        i++
+    }
 
-    return [synonyms, related, near_antonyms, antonyms];
+    return synonyms, related, near_antonyms, antonyms, i, reading.length;    
 }
 
 module.exports = counting;
@@ -30,6 +35,7 @@ if (require.main === module) {
           console.error(err)
           return
         }
-        
+        text = data.split(/\s+/g);
+        console.log(text)
       })
 }
