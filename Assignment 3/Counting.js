67c3462b27fadacfcb55529b7975b31de44f2d20
pikakight <tmmarcus73@gmail.com>
2021-12-08 22:10:45 -0500

    Got something working

--- Assignment 3/Counting.js
+++ Assignment 3/Counting.js
@@ -1,23 +1,20 @@
-const { count } = require("console");
-
 function counting(reading){
     const words = require("./words.json")
     let [synonyms,related,near_antonyms,antonyms] = [0,0,0,0]; 
-    i = 0;
-    for (element of reading){
-        if (element in words["Synonyms"]){
+    for (i = 0; i < reading.length; i++){
+        reading[i] = reading[i].replace("(", "").replace(")", "").replace("\"", "").replace(",","").replace(".","").replace(":","").replace("...", "").replace("?","").replace(";", "");
+        if (reading[i] in words["Synonyms"]){
             synonyms += 1;
         }
-        else if (element in words["Related"]){
+        if (reading[i] in words["Related"]){
             related +=1;
         }
-        else if (element in words["Near Antonyms"]){
+        if (reading[i] in words["Near Antonyms"]){
             near_antonyms += 1;
         }
-        else if (element in words["Antonyms"]){
+        if (reading[i] in words["Antonyms"]){
             antonyms += 1;
         }
-        i++
     }
 
     return [synonyms, related, near_antonyms, antonyms];    
